1db0e16f3e8afdde4aba241b8b76bdeaed2bf970
Kelvin Goodman <kelvin12goodman@gmail.com>
2016-07-15 23:02:51 -0400

    Added quadrants

--- js/FlightChart.js
+++ js/FlightChart.js
@@ -1,16 +1,14 @@
 var img = new Image();
 img.src = './img/plane.jpg';
 img.onload = function() {
-var ctx = document.getElementById('myChart').getContext('2d');
+var ctx = document.getElementById('quad1Chart').getContext('2d');
 var fillPattern = ctx.createPattern(img, 'no-repeat');
-var myChart = new Chart(ctx, {
+var quad1Chart = new Chart(ctx, {
     type: 'line',
     data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
         datasets: [{
-            label: '# of Votes',
-            data: [{x:5,y:5},{x:7,y:5},{x:5,y:4},],
-            borderWidth: 1,
+            data: [{x: 8,y: 0}, {x: 5,y: 10}, {x: 10,y: 5}],
+			borderWidth: 1,
 			pointStyle: ('image', img),
 			radius: 50,
 			fill: false,
@@ -19,15 +17,148 @@ var myChart = new Chart(ctx, {
     },
     options: {
         scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
+            xAxes: [{
+                type: 'linear',
+                display: false,
+				ticks: {
+                    max: 15,
+                    min: -10,
+                    stepSize: 5
                 }
             }],
-			xAxes: [{
-                ticks: {
-                    type: 'linear',
-					position: 'top'
+			yAxes: [{
+                type: 'linear',
+                display: false,
+				ticks: {
+                    max: 15,
+                    min: -10,
+                    stepSize: 5
+                }
+            }]
+        }
+    }
+});
+}
+
+var img = new Image();
+img.src = './img/plane.jpg';
+img.onload = function() {
+var ctx = document.getElementById('quad2Chart').getContext('2d');
+var fillPattern = ctx.createPattern(img, 'no-repeat');
+var quad1Chart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        datasets: [{
+            data: [{x: 8,y: 0}, {x: 5,y: 10}, {x: 10,y: 5}],
+			borderWidth: 1,
+			pointStyle: ('image', img),
+			radius: 50,
+			fill: false,
+			borderColor: "rgba(0,0,0,0)"
+        }]
+    },
+    options: {
+        scales: {
+            xAxes: [{
+                type: 'linear',
+                display: false,
+				ticks: {
+                    max: 15,
+                    min: -10,
+                    stepSize: 5
+                }
+            }],
+			yAxes: [{
+                type: 'linear',
+                display: false,
+				ticks: {
+                    max: 15,
+                    min: -10,
+                    stepSize: 5
+                }
+            }]
+        }
+    }
+});
+}
+
+var img = new Image();
+img.src = './img/plane.jpg';
+img.onload = function() {
+var ctx = document.getElementById('quad3Chart').getContext('2d');
+var fillPattern = ctx.createPattern(img, 'no-repeat');
+var quad1Chart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        datasets: [{
+            data: [{x: 8,y: 0}, {x: 5,y: 10}, {x: 10,y: 5}],
+			borderWidth: 1,
+			pointStyle: ('image', img),
+			radius: 50,
+			fill: false,
+			borderColor: "rgba(0,0,0,0)"
+        }]
+    },
+    options: {
+        scales: {
+            xAxes: [{
+                type: 'linear',
+                display: false,
+				ticks: {
+                    max: 15,
+                    min: -10,
+                    stepSize: 5
+                }
+            }],
+			yAxes: [{
+                type: 'linear',
+                display: false,
+				ticks: {
+                    max: 15,
+                    min: -10,
+                    stepSize: 5
+                }
+            }]
+        }
+    }
+});
+}
+
+var img = new Image();
+img.src = './img/plane.jpg';
+img.onload = function() {
+var ctx = document.getElementById('quad4Chart').getContext('2d');
+var fillPattern = ctx.createPattern(img, 'no-repeat');
+var quad1Chart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        datasets: [{
+            data: [{x: 8,y: 0}, {x: 5,y: 10}, {x: 10,y: 5}],
+			borderWidth: 1,
+			pointStyle: ('image', img),
+			radius: 50,
+			fill: false,
+			borderColor: "rgba(0,0,0,0)"
+        }]
+    },
+    options: {
+        scales: {
+            xAxes: [{
+                type: 'linear',
+                display: false,
+				ticks: {
+                    max: 15,
+                    min: -10,
+                    stepSize: 5
+                }
+            }],
+			yAxes: [{
+                type: 'linear',
+                display: false,
+				ticks: {
+                    max: 15,
+                    min: -10,
+                    stepSize: 5
                 }
             }]
         }
